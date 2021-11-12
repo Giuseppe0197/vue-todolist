ea895a367f02bcb9d728b17b178c29f67c57c85d
Giuseppe0197 <faro.g@hotmail.it>
2021-11-12 14:43:27 +0100

    effetto cancellazione items

--- js/myscript.js
+++ js/myscript.js
@@ -2,8 +2,89 @@
 
 /* andiamo a collegare in vue il contenitore dei vari elementi che varranno stampati */
 
-/* andiamo a creare un'array di oggetti per stampare gli elementi */
+var containerTasks  = new Vue (
 
-/* andiamo a creare delle funzioni per cui l'utente può barrare le task già fatte */
+    {
 
-/* andiamo a creare una funzione per far aggiungere all'utente le proprie task */
+        el: "#container",
+
+        data: {
+
+            newTask:{
+                text: "",
+                done: "false"
+            },
+
+            /* andiamo a creare un'array di oggetti per stampare gli elementi */
+
+            tasks: [
+
+                {
+                    text: "Preparare il pranzo",
+                    done: false
+                },
+
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+
+                {
+                    text: "Fare il bucato",
+                    done: false
+                },
+
+                {
+                    text: "Studiare",
+                    done: false
+                },
+
+                {
+                    text: "Andare in palestra",
+                    done: false
+                },
+
+                {
+                    text: "Uscire con gli amici",
+                    done: false
+                },
+
+            ]
+
+        },
+
+        /* andiamo a creare delle funzioni per cui l'utente può barrare le task già fatte */
+
+        methods: {
+
+            addLine(){
+
+                this.tasks.done = true;
+
+            },
+
+            removeTask(index){
+
+                this.tasks.splice(index, 1);
+
+            },
+
+            /* andiamo a creare una funzione per far aggiungere all'utente le proprie task */
+
+            addTask(){
+
+                this.tasks.push(this.newTask);
+                this.newTask = {
+
+                    text: "",
+                    done: false
+
+                }
+
+            }
+
+        }
+
+    }
+
+)
