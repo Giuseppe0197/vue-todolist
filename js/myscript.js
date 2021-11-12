741db7802c7a848c70a49d9e3e5e1c4c611e2c44
Giuseppe0197 <faro.g@hotmail.it>
2021-11-12 13:32:37 +0100

    impostazione esercizio

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+/* creare una todolist in cui l'utente può barrare le cose già fatte e inserire nuove task */
+
+/* andiamo a collegare in vue il contenitore dei vari elementi che varranno stampati */
+
+/* andiamo a creare un'array di oggetti per stampare gli elementi */
+
+/* andiamo a creare delle funzioni per cui l'utente può barrare le task già fatte */
+
+/* andiamo a creare una funzione per far aggiungere all'utente le proprie task */
